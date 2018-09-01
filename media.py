f746f31943eb3f1bf0da3c040e22d7bbd74affbc
wally6 <42121816+wally6@users.noreply.github.com>
2018-09-01 12:30:15 -0400

    Update media.py

--- media.py
+++ media.py
@@ -2,19 +2,20 @@ import webbrowser
 import turtle
 
 class Movie():
-    def __init__ (self, movie_title, moive_storyline, poster_image, trailer_youtube):
+    def __init__ (self, movie_title, movie_storyline, poster_image, trailer_youtube):
         self.title = movie_title
         self.storyline = movie_storyline
         self.poster_image_url = poster_image
         self.trailer_youtube_url = trailer_youtube
 
+        VALID_RATINGS = ["G",
+                        "PG",
+                        "PG-13",
+                        "R"]
+
 def show_trailer(self):
     webbrowser.open(self.trailer_youtube_url)
 
 def draw_square():
-    windows = turtle.Screen()
-    window.bgcolor("red")
-    w = turtle.Turtle()
-    w.forward(100)
 
     window.exitonclick()
