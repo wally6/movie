f07a4583db369e7dc95f68a4e261dabfa3a37ece
wally6 <42121816+wally6@users.noreply.github.com>
2018-09-16 13:21:34 -0400

    Update media.py

--- media.py
+++ media.py
@@ -1,21 +1,36 @@
+""" Defines the Movie class that contains the details of a movie."""
 import webbrowser
 import turtle
 
-class Movie():
-    def __init__ (self, movie_title, movie_storyline, poster_image, trailer_youtube):
+class Movie ():
+    """This class provides a way to save and store movies trailers
+
+Attributes:
+        title: A string to store the title of movies.
+        storyline: A string to store the basic plot of movies.
+        poster: A string to store the URL images
+        trailer: string to store the URL movie links"""
+    
+
+    def __init__ (self, movie_title, movie_storyline, \
+                  poster_image, trailer_youtube):
         self.title = movie_title
-        self.storyline = movie_storyline
+        self.storyline = movie_storyline 
         self.poster_image_url = poster_image
         self.trailer_youtube_url = trailer_youtube
 
-        VALID_RATINGS = ["G",
+""" reatings: show ratings"""
+VALID_RATINGS = ["G",
                         "PG",
                         "PG-13",
                         "R"]
 
-def show_trailer(self):
+
+def show_trailer (self):
     webbrowser.open(self.trailer_youtube_url)
+    """ plays the movie trailer in the web browser."""
 
-def draw_square():
 
-    window.exitonclick()
+def draw_square ():
+    window.exitonclick ()
+    """ shows box line in the web browser."""
