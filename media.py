2dc049a7b5d1e8b70f69da334483bf25fa4e5d07
wally6 <42121816+wally6@users.noreply.github.com>
2018-10-20 13:22:34 -0400

    Update media.py

--- media.py
+++ media.py
@@ -1,37 +1,34 @@
 """ Defines the Movie class that contains the details of a movie."""
 import webbrowser
 import turtle
-import math, sys;
+
 
 class Movie ():
-"""This class provides a way to save and store movies trailers
+    """This class provides a way to save and store movies trailers."""
 
 Attributes:
-        title: A string to store the title of movies.
-        storyline: A string to store the basic plot of movies.
-        poster: A string to store the URL images
-        trailer: string to store the URL movie links"""
-    
+        """title: A string to store the title of movies."""
+        """storyline: A string to store the basic plot of movies."""
+        """poster: A string to store the URL images."""
+        """trailer: string to store the URL movie links"""
 
-    def __init__ (self, movie_title, movie_storyline, \
-                  poster_image, trailer_youtube):
+
+def __init__
+(self, movie_title, movie_storyline, poster_image, trailer_youtube):
         self.title = movie_title
-        self.storyline = movie_storyline 
+        self.storyline = movie_storyline
         self.poster_image_url = poster_image
         self.trailer_youtube_url = trailer_youtube
 
-""" reatings: show ratings"""
-VALID_RATINGS = ["G",
-                        "PG",
-                        "PG-13",
-                        "R"]
 
 """ plays the movie trailer in the web browser."""
-def show_trailer (self):
+
+
+def show_trailer(self):
     webbrowser.open(self.trailer_youtube_url)
-    
-    
+
+
 """ shows box line in the web browser."""
-def draw_square ():
-    window.exitonclick ()
-    
+
+
+def draw_square(): window.exitonclick()
